0a4d765ac6ff646fd088db5a8f0364c8d19aab28
Anisha Chhatwani <chhatwania1@udayton.edu>
2023-01-26 01:16:24 -0500

    initial setup

---
+++ Frontend-React/src/layouts/authentication/sign-in/index.js
@@ -0,0 +1,174 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Switch from "@mui/material/Switch";
+// import Grid from "@mui/material/Grid";
+// import MuiLink from "@mui/material/Link";
+
+// @mui icons
+// import FacebookIcon from "@mui/icons-material/Facebook";
+// import GitHubIcon from "@mui/icons-material/GitHub";
+// import GoogleIcon from "@mui/icons-material/Google";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDInput from "components/MDInput";
+import MDButton from "components/MDButton";
+
+// Authentication layout components
+import BasicLayout from "layouts/authentication/components/BasicLayout";
+
+// Images
+import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+
+function Basic() {
+  const [rememberMe, setRememberMe] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSetRememberMe = () => setRememberMe(!rememberMe);
+
+  const handleSignIn = () => {
+    if (email === "") {
+      setEmailError("Email Address cannot be empty!");
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+      setEmailError("Incorrect Email Address!");
+    } else if (password === "") {
+      setPasswordError("Password cannot be empty!");
+    } else if (password.length < 6) {
+      setPasswordError("Password cannot be less than 6 characters!");
+    } else if (password.length > 30) {
+      setPasswordError("Password cannot be greater than 30 characters!");
+    }
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+    if (event.target.value !== "") {
+      setEmailError("");
+    }
+  };
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value !== "") {
+      setPasswordError("");
+    }
+  };
+
+  return (
+    <BasicLayout image={bgImage}>
+      <Card>
+        <MDBox
+          variant="gradient"
+          bgColor="info"
+          borderRadius="lg"
+          coloredShadow="info"
+          mx={2}
+          mt={-3}
+          p={2}
+          mb={1}
+          textAlign="center"
+        >
+          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+            Sign in
+          </MDTypography>
+        </MDBox>
+        <MDBox pt={4} pb={3} px={3}>
+          <MDBox component="form" role="form">
+            <MDBox mb={2}>
+              <MDInput
+                type="email"
+                label="Email"
+                fullWidth
+                value={email}
+                onChange={handleEmail}
+                error={emailError}
+              />
+            </MDBox>
+            <MDBox mb={2}>
+              <MDInput
+                type="password"
+                label="Password"
+                fullWidth
+                value={password}
+                onChange={handlePassword}
+                error={passwordError}
+              />
+            </MDBox>
+            <MDBox display="flex" alignItems="center" ml={-1}>
+              <Switch checked={rememberMe} onChange={handleSetRememberMe} />
+              <MDTypography
+                variant="button"
+                fontWeight="regular"
+                color="text"
+                onClick={handleSetRememberMe}
+                sx={{ cursor: "pointer", userSelect: "none", ml: -1 }}
+              >
+                &nbsp;&nbsp;Remember me
+              </MDTypography>
+            </MDBox>
+            {emailError !== "" && (
+              <MDBox mb={2}>
+                <MDTypography variant="button" color="error">
+                  {emailError}
+                </MDTypography>
+              </MDBox>
+            )}
+            {passwordError !== "" && (
+              <MDBox mb={2}>
+                <MDTypography variant="button" color="error">
+                  {passwordError}
+                </MDTypography>
+              </MDBox>
+            )}
+            <MDBox mt={4} mb={1}>
+              <MDButton variant="gradient" color="info" fullWidth onClick={handleSignIn}>
+                sign in
+              </MDButton>
+            </MDBox>
+            <MDBox mt={3} mb={1} textAlign="center">
+              <MDTypography variant="button" color="text">
+                Don&apos;t have an account?{" "}
+                <MDTypography
+                  component={Link}
+                  to="/authentication/sign-up"
+                  variant="button"
+                  color="info"
+                  fontWeight="medium"
+                  textGradient
+                >
+                  Sign up
+                </MDTypography>
+              </MDTypography>
+            </MDBox>
+          </MDBox>
+        </MDBox>
+      </Card>
+    </BasicLayout>
+  );
+}
+
+export default Basic;
---
+++ Frontend-React/src/utility.js
@@ -0,0 +1,24 @@
+function validateEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
+// function validateEmail(mail) {
+//   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+//     return true
+//   }
+//   return false
+// }
+
+function validatePassword(password) {
+  return String(password)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
+export { validateEmail, validatePassword };
